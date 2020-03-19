a56a149cf6761fc9d4654c290b650a42c9b5e144
agumusay <a.gumusay@protonmail.com>
2020-03-19 12:28:46 +0100

    localStoreage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,22 @@
 import "../styles/main.scss";
 import "regenerator-runtime/runtime";
-
-let themeColor = localStorage.getItem("theme");
-
+let body = document.body;
+let themeColorInitial = localStorage.getItem("theme");
+console.log(themeColorInitial);
+if (themeColorInitial) {
+  body.dataset.theme = themeColorInitial;
+}
+if (themeColorInitial === "light") {
+  document.querySelector(".switch").checked = false;
+} else {
+  document.querySelector(".switch").checked = true;
+}
 document.querySelector(".switch").addEventListener("click", () => {
   if (event.target.checked) {
-    document.body.dataset.theme = themeColor;
+    document.body.dataset.theme = "light";
   } else {
-    document.body.dataset.theme = themeColor;
+    document.body.dataset.theme = "dark";
   }
-
-  localStorage.setItem("theme", document.body.dataset.theme);
+  let themeValue = document.body.dataset.theme;
+  localStorage.setItem("theme", themeValue);
 });
